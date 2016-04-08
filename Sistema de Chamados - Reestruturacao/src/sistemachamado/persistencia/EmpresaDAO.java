77bcdbd2f26391965be19e1d86469692b8d3664f
41414268 <41414268@099149-P31S401.macklabs.br>
2016-04-08 10:32:01 -0300

    Sistema de Chamada - Reestruturado

---
+++ Sistema de Chamados - Reestruturacao/src/sistemachamado/persistencia/EmpresaDAO.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistemachamado.persistencia;
+
+/**
+ *
+ * @author 41414268
+ */
+public class EmpresaDAO {
+    
+}
